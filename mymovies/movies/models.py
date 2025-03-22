27326db81a3be1ceff03b28ccf952da3828d1309
marisamak <marinasamakova1507@gmail.com>
2025-03-22 16:46:21 +0300

    add

---
+++ mymovies/movies/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class Movie(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    image = models.ImageField(upload_to='movie_images/')
+
+    def __str__(self):
+        return self.title
+
